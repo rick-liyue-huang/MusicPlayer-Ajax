fa31c96a1773ab0098db589135155efafcfb410f
rick.liyue.huang <rick.liyue.huang@gmail.com>
2018-10-01 16:12:33 +1000

    add ajax2 and modify index.html

---
+++ intro ajax/xhr2.php
@@ -0,0 +1,28 @@
+<?php
+	
+	/*sleep(2); // simulate timeout
+
+	echo '{
+
+		"name": "leo"
+	}';*/
+
+	// echo $_FILES['file1'];
+	// print_r($_FILES['file1']);
+
+	$index = $_GET['index'];
+	sleep($index);
+	echo $index;
+
+
+
+
+
+
+
+
+
+
+
+
+?>
